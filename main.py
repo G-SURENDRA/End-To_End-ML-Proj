26ffa94b444fe769f094ec6442991345b64948a9
G-SURENDRA <surendragara999@gmail.com>
2025-09-19 16:55:55 +0530

    data transformation done

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from mlProject.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage02_data_validation import DataValidationTrainingPipeline
+from mlProject.pipeline.stage03_data_transformation import DataTransformationTrainingPipeline
 from mlProject import logger
 
 
-# STAGE_NAME = "Data Ingestion stage"
-# try:
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-#    data_ingestion = DataIngestionTrainingPipeline()
-#    data_ingestion.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
 
@@ -23,6 +24,20 @@ if __name__== '__main__':
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
         data_validation= DataValidationTrainingPipeline()
         data_validation.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+            logger.exception(e)
+            raise e
+    
+
+
+STAGE_NAME = "Data Transformation stage"
+
+if __name__== '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+        data_transformation = DataTransformationTrainingPipeline()
+        data_transformation.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
             logger.exception(e)
